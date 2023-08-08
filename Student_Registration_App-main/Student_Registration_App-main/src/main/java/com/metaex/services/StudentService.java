51fedbd06d8df8f4e5fe8f0ba66886c5c0bbf7e0
piyush karmakar <79916156+piyushkarmakar@users.noreply.github.com>
2023-08-08 12:50:26 +0530

    Add files via upload

---
+++ Student_Registration_App-main/Student_Registration_App-main/src/main/java/com/metaex/services/StudentService.java
@@ -0,0 +1,27 @@
+package com.metaex.services;
+
+import java.util.List;
+
+import com.metaex.entites.Student;
+
+public interface StudentService {
+
+	public void saveStudent(Student student);
+
+	public List<Student> getStudents();
+	
+	public void deleteStudent(long id);
+	public Student findStudentId(long id);
+	
+	public void saveOneStudent(Student student);
+	
+	
+	
+	
+	
+	
+	
+	
+ 
+
+}
